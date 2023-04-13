abfdd49ab83d9f5a6f84eed19ec6b1a34b0878e9
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-04-13 10:18:14 +0200

    Add project files.

---
+++ ProteinTrackerAPI/Model/Food.cs
@@ -0,0 +1,12 @@
+﻿namespace ProteinTrackerAPI.Model
+{
+    public class Food
+    {
+        private int _id;
+        private string _name;
+        private int _protein;
+        private int _kcal;
+        private int _idOfConsumer;
+        private DateTime _consumptionDateTime;
+    }
+}
---
+++ ProteinTrackerAPI/Model/User.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+
+namespace ProteinTrackerAPI.Model
+{
+    public class User
+    {
+        private int _id;
+        private string _name;
+        private string _gender;
+        private string _userName;
+        private string _password;
+        private int _weight;
+
+        public List<User> MakeUserData()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User()
+            {
+                _id = _id,
+                _name = _name,
+                _gender = _gender,
+                _userName = _userName,
+                _password = _password,
+                _weight = _weight
+            });
+            string json = JsonSerializer.Serialize(users);
+        }
+
+    }
+}
